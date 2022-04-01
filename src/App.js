5fa5327086013c792f8735aba60eb58f5dafccf5
Oledgaman <obulaew@yandex.ru>
2022-04-01 23:25:28 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './Components/Counter';
+import "./styles/App.css"; 
 
 function App() {
+  const  [textiput, settextinput] = useState('Текстовые данные')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter/>
+      <h1>{textiput}</h1>
+      <input 
+      type="text" 
+      value={textiput} 
+      onChange={event => settextinput(event.target.value)}/>  <br/><br/>
+        <div className="post">
+          <div className="post__container">
+            <strong> JS JS JS</strong>
+            <div>
+               JS - is hard
+            </div>
+          </div>
+          <div className="post__btns" >
+            <button>Удалить</button>
+          </div>
+        </div>
     </div>
+    
   );
 }
 
